3ac50eff32dcdcc2ef4d7245294af2e87f703db3
imthaghost <imthaghost@protonmail.com>
2021-03-19 00:22:53 -0700

    [update] binance buy functionality

--- pkg/broker/binance/account.go
+++ pkg/broker/binance/account.go
@@ -1,13 +1,36 @@
 package binance
 
+import (
+	"context"
+	"fmt"
+	"github.com/adshao/go-binance/v2"
+)
 
-// Buy ...
-func (b *BrokerService) Buy(ticker string) {
+// BuyAll ...
+func (b *BrokerService) BuyAll(ticker string) {
+	// build order request
+	order, err := b.Client.NewCreateOrderService().Symbol(ticker).
+		Side(binance.SideTypeBuy).Type(binance.OrderTypeLimit).
+		TimeInForce(binance.TimeInForceTypeGTC).Quantity("5").
+		Price("0.0030000").Do(context.Background())
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(order)
+}
+
+// SellAll ...
+func (b *BrokerService) SellAll(ticker string) {
 
 }
 
-// Sell ...
-func (b *BrokerService) Sell(ticker string) {
+// Sell ..
+func (b *BrokerService) Sell(ticker string, shares float64) {
+
+}
+// Buy ...
+func (b *BrokerService) Buy(ticker string, shares float64) {
 
 }
 
